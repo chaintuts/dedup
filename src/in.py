c608a58ad65cc66fb730376962d1200f61dc9f78
Josh McIntyre <josh@jmcintyre.net>
2021-03-17 11:41:08 -0400

    Add a script for checking if all items in one directory are also in another (set intersection)

---
+++ src/in.py
@@ -0,0 +1,61 @@
+# This file contains code for finding file diffs by content in 2 different directory
+# Useful for syncing files manually
+#
+# Author: Josh McIntyre
+#
+import os
+import hashlib
+import argparse
+
+# Build the directory hash listing
+def find_hashes(directory):
+
+    # Get a file listing
+    os.chdir(directory)
+    names = [ name for name in os.listdir(directory) if os.path.isfile(name) ]
+
+    # Create a dict of file hashes, names
+    hashes = {}
+    for name in names:
+        with open(name, "rb") as f:
+            content = f.read()
+            digest = hashlib.sha256(content).hexdigest()
+            hashes[digest] = name
+
+    return hashes
+
+# Calculate the list of files that are in 1 but not in 2
+def find_diff(hashes, hashes2):
+
+    inter = set(hashes).intersection(set(hashes2))
+    diffset = set(hashes) - inter
+    diff = { k : hashes[k] for k in diffset }
+    return diff
+
+# Output the found duplicates
+def output_diff(diff):
+
+    for digest, name in diff.items():
+        print("Found file from 1 not in 2 -  " + digest[:8] + ": " + name)
+
+# The main entry point for the program
+def main():
+
+    # Fetch the directory from the command line args
+    parser = argparse.ArgumentParser(description="Find duplicate files by content")
+    parser.add_argument("directory", type=str, help="The first directory to compare")
+    parser.add_argument("directory2", type=str, help="The second directory to compare")
+    args = parser.parse_args()
+
+    # Generate the listing
+    hashes = find_hashes(args.directory)
+    hashes2 = find_hashes(args.directory2)
+    
+    # Calculate the diff
+    diff = find_diff(hashes, hashes2)
+    
+    # Output the duplicates list
+    output_diff(diff)
+
+if __name__ == "__main__":
+    main()
